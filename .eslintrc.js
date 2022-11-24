fab01b81f1306677b66b15e7627a1f2ff1a17552
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2022-11-24 15:18:42 +0200

    install and config jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
   extends: 'airbnb-base',
   overrides: [
@@ -12,5 +13,7 @@ module.exports = {
   },
   rules: {
     'no-console': 'off',
+    'linebreak-style': 'off',
+    'no-useless-return': 'warn',
   },
 };
