be4a34107856376ca5d517d40e5d1d3f8fc93241
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2022-11-27 14:47:01 +0200

    Temp

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+import { initBoard, renderBoard } from './dom';
+import newGame from './game';
+
+import './style.css';
+
 console.log('Hello World!');
+
+const playerBoard = document.querySelector('.defence-board');
+const computerBoard = document.querySelector('.attack-board');
+
+const { player, computer } = newGame();
+
+initBoard(player.gridSize, playerBoard);
+initBoard(computer.gridSize, computerBoard);
+renderBoard(player, playerBoard);
+renderBoard(computer, computerBoard, false);
