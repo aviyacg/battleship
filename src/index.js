0b880414c9774602904292aa923e21731ccf8181
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2022-11-27 23:56:55 +0200

    Finish game logic

--- src/index.js
+++ src/index.js
@@ -3,30 +3,45 @@ import { newGame, getComputerMove } from './game';
 
 import './style.css';
 
-console.log('Hello World!');
-
 const playerBoard = document.querySelector('.defence-board');
 const computerBoard = document.querySelector('.attack-board');
 
-const { player, computer } = newGame();
+document.querySelector('button').addEventListener('click', () => {
+  // init game
+  document.querySelector('button').textContent = 'New Game';
+  const { player, computer } = newGame();
+  initBoard(player.gridSize, playerBoard);
+  initBoard(computer.gridSize, computerBoard);
 
-initBoard(player.gridSize, playerBoard);
-initBoard(computer.gridSize, computerBoard);
+  renderBoard(player, playerBoard);
+  renderBoard(computer, computerBoard, false);
 
-computerBoard.querySelectorAll('.square').forEach(
-  (square) => square.addEventListener('click', (event) => {
-    let { x, y } = event.target.dataset;
-    x = parseInt(x);
-    y = parseInt(y);
-    computer.recieveAttack(x, y);
-    renderBoard(computer, computerBoard, false);
-
-    if (!computer.isAllSunk()) {
-      getComputerMove(player);
-      renderBoard(player, playerBoard);
-    }
-  }),
-);
-
-renderBoard(player, playerBoard);
-renderBoard(computer, computerBoard, false);
+  // the event listener callback manage all gameplay logic
+  computerBoard.querySelectorAll('.square').forEach(
+    (square) => square.addEventListener('click', (event) => {
+      if (player.isAllSunk() || computer.isAllSunk()) return;
+      // get coordinates
+      let { x, y } = event.target.dataset;
+      x = parseInt(x);
+      y = parseInt(y);
+      // check if attack is valid
+      const isValide = computer.recieveAttack(x, y);
+      if (isValide !== undefined) {
+        // render player move
+        renderBoard(computer, computerBoard, false);
+        // check if player won
+        if (computer.isAllSunk()) {
+          document.querySelector('button').textContent = 'You won, Play agian!';
+          return;
+        }
+        // computer move
+        getComputerMove(player);
+        renderBoard(player, playerBoard);
+        // check if computer won
+        if (player.isAllSunk()) {
+          document.querySelector('button').textContent = 'You lose, Try again!';
+        }
+      }
+    }),
+  );
+});
